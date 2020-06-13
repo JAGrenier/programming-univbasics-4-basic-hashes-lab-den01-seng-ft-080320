7e0ac497dd6a155883c91befdb09dad2198a40b8
Jennifer Grenier <jenniferagrenier@gmail.com>
2020-06-13 01:09:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -12,11 +12,8 @@ def pioneer
   {name: 'Grace Hopper'}
 end
 
-
 def id_hash_generator(number)
-  hash= {
-    :id => number
-  }
+  {id:number}
 
   # return a hash with a key :id assigned to the provided number
 end
